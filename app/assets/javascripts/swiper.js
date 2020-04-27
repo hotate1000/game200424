924fa79f0d54b0178760d70e4a0d40a84da64f75
hotate1000 <ooo.336.sss@gmail.com>
2020-04-27 17:33:19 +0900

    ランニングゲーム実装①JSファイル変更

--- app/assets/javascripts/swiper.js
+++ app/assets/javascripts/swiper.js
@@ -1,18 +1,18 @@
-window.addEventListener('DOMContentLoaded', function() {
-	var slider1 = new Swiper ('.slider1', {
-		loop: true,
-		initialSlide: -1,
-		spaceBetween: -50,
-		slidesPerView: 3,
-		speed: 500,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
-	});
-}, false);
+// window.addEventListener('DOMContentLoaded', function() {
+// 	var slider1 = new Swiper ('.slider1', {
+// 		loop: true,
+// 		initialSlide: -1,
+// 		spaceBetween: 20,
+// 		slidesPerView: 3,
+// 		speed: 500,
+// 		navigation: {
+// 			nextEl: '.swiper-button-next',
+// 			prevEl: '.swiper-button-prev'
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       type: 'bullets',
+//       clickable: true,
+//     },
+// 	});
+// }, false);
