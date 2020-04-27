f41234571fd82227002ac2cfdf290434651658c7
hotate1000 <ooo.336.sss@gmail.com>
2020-04-27 14:55:02 +0900

    ブロック崩しゲーム実装①JQの実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,9 +2,9 @@ class GamesController < ApplicationController
   def index
   end
 
-  def runninggame
-  end 
-
   def blockbreaking
   end
+
+  def runningman
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'games#index'
   resource:games do
     collection do
-      get "runninggame"
       get "blockbreaking"
+      get "runningman"
     end
   end
 end
